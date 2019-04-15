56cefc72196875f6d8ef50cabfed30d352eda268
Alexander Swensen <alex.swensen@gmail.com>
2019-04-14 20:12:26 -0500

    app connect magic (learn more about this)

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import { render } from "react-dom";
 import css from "./styles/style.styl";
-import Main from "./components/Main";
+import App from "./components/App";
 import Single from "./components/Single";
 import PhotoGrid from "./components/PhotoGrid";
 
 // Import react-router deps
 
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Router, Route, IndexRoute } from "react-router";
 
 import { Provider } from "react-redux";
 import store, { history } from "./store";
@@ -16,7 +16,7 @@ import store, { history } from "./store";
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid} />
         <Route path="/view/:postId" component={Single} />
       </Route>
